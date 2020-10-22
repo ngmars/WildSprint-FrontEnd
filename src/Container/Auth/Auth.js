54dab352494af1da3bed4285410a0baa4f8688c8
Nitish <nitishgopinath@gmail.com>
2020-10-22 16:45:26 +0530

    kill ze tanyaaaa

--- src/Container/Auth/Auth.js
+++ src/Container/Auth/Auth.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-
 import Input from '../../Components/UI/Input/Input';
 import Button from '../../Components/UI/Button/Button';
+import classes from './Auth.css';
 class Auth extends Component{
     state={
         controls:{
@@ -35,6 +35,51 @@ class Auth extends Component{
                  }
         }
     }
+    checkValidity(value, rules) {
+        let isValid = true;
+        if (!rules) {
+            return true;
+        }
+        
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        return isValid;
+    }
+
+    inputChangedHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                touched: true
+            }
+        };
+        this.setState({controls: updatedControls});
+    }
+
+
 
     render(){
         const formElementsArray=[];
@@ -58,9 +103,9 @@ class Auth extends Component{
 
         return (
             
-            <div>
-                <form>
-                    {form}
+            <div className={classes.Auth}>
+                <form onSubmit={this.submitHandler}>
+                    {form }
                     <Button btnType="Success">Login</Button>
                 </form>
             </div>
