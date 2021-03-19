05a434d5c0ded911864441f91437679491db90ae
Nitish <nitishgopinath@gmail.com>
2021-03-20 01:18:08 +0530

    updated dyte

--- src/Container/Dashboard/Dyte/Dyte.js
+++ src/Container/Dashboard/Dyte/Dyte.js
@@ -1,42 +1,57 @@
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { DyteMeeting, Meeting } from "dyte-client";
 
 import axios from 'axios';
 
 export default function DyteMeet() {
-    React.useEffect(() => {
-        const options = {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': '069fe49b1f4c0d45b4b4'
-              },
-              body: JSON.stringify({authorization: {waitingRoom: false}, title: 'TestMeeting'})
-          }
-        axios.post('https://api.cluster.dyte.in/v1/organizations/959e35cc-d5bd-4fcd-9fc2-c0d3690d5f97/meeting',options)
-        .then(res=>{
-         console.log("NEWWW RESPONSE",res.data)
-        
-                }).catch(err=>{
-                   console.log('ERROR',err)
-                })
-    },[])
+    const [roomDeet,setRoomDeet] = useState({});
+    const [roomName,setRoomName] = useState(null);
+    useEffect(() => {
+        const fetch = require('node-fetch');
 
-    return (
-        <div className="App">
-            <DyteMeeting
+const url = 'https://api.cluster.dyte.in/v1/organizations/959e35cc-d5bd-4fcd-9fc2-c0d3690d5f97/meeting';
+
+const options = {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: '069fe49b1f4c0d45b4b4'
+  },
+  body: JSON.stringify({authorization: {waitingRoom: false}, title: 'TestMeetingNew'})
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => {
+      console.log(json)
+      setRoomDeet(json.data.meeting);
+      setRoomName(json.data.meeting.roomName)
+    })
+  .catch(err => console.error('error:' + err));
+    },[])
+    let DyteComponent;
+if(roomName!==null){
+    console.log('ROOM NAME',roomName)
+    DyteComponent= (
+<DyteMeeting
                     onInit={(meeting) => Meeting}
-                    clientId="959e35cc-d5bd-4fcd-9fc2-c0d3690d5f97"
+                    clientId={roomDeet.id}
                      uiConfig={{
                                     header:false
                                 }}
                     meetingConfig={{
-                        roomName: 'TestMeeting',
+                        roomName: {roomName},
                         authToken: '069fe49b1f4c0d45b4b4',
                         showSetupScreen: true
                     }}
             />
+    )
+}
+    return (
+        <div className="App">
+            {DyteComponent}
         </div>
         );
 }
